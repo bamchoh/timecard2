19459aacffb18fa5c116eb7612517dab2f9e75b5
bamchoh <bamchoh@gmail.com>
2019-04-30 16:13:42 +0900

    Make signup sequence

--- src/App.js
+++ src/App.js
@@ -19,63 +19,174 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      init: null,
+      page: "init",
       user: null,
     };
 
     firebase.auth().onAuthStateChanged((user) => {
+      if(this.state.page === "signout") {
+        return
+      }
+
+      console.log(user);
       if (user) {
         if(!user.emailVerified) {
-          user.sendEmailVerification().then(function() {
-            console.log("email sent");
-          }).catch(function(error) {
-            console.log("sendEmailVerification error");
-            console.log(error);
-          });
+          console.log("email is not verified yet");
+          if(this.state.page === "signup") {
+            this.sendEmailVerification(user)
+          }
+          this.setState({page: "verification", user: user})
         } else {
-          this.setState({init:true, user: user});
+          if(!user.displayName) {
+            this.setState({page:"first_login", user: user})
+          } else {
+            this.setState({page:"timecard", user: user});
+          }
         }
       } else {
         console.log("no user");
-        this.setState({init:true, user: null});
+        this.setState({page:"login", user: null});
       }
     });
 
   }
 
+  sendEmailVerification = (user) => {
+    user.sendEmailVerification().then(() => {
+      console.log("email sent");
+    }).catch(function(error) {
+      console.log("sendEmailVerification error");
+      console.log(error);
+    });
+  }
+
   signIn = (e) => {
-    this.setState({init: null});
   }
 
   signUp = (e) => {
+    this.setState({page:"signup"})
   }
 
   signOut = () => {
+    this.setState({page:"signout"})
     firebase.auth().signOut().then(function() {
       console.log("succeeded sign-out");
     }).catch(function(error) {
       console.log("signOut error");
       console.log(error);
     });
+    this.reloadPage()
+  }
+
+  reloadPage = () => {
+    window.location.reload();
   }
 
   render() {
-    if(this.state.init == null) {
-      return(<LoadingPage />)
+    switch(this.state.page) {
+      case "first_login":
+        return(<FirstLoginPage user={this.state.user} changePage={this.reloadPage} />)
+      case "timecard":
+        return(<TimeCardPage user={this.state.user} handleSignOut={this.signOut} />)
+      case "login":
+        return(<LoginDialog handleLogIn={this.signIn} handleSignUp={this.signUp} />)
+      case "signup":
+        return(<SignupPage />)
+      case "verification":
+        return(<VerificationPage user={this.state.user} changePage={this.reloadPage} handleResend={this.sendEmailVerification} />)
+      default:
+        return(<LoadingPage />)
     }
+  }
+}
 
-    if(this.state.user) {
-      return(<TimeCardPage
-        user={this.state.user}
-        handleSignOut={this.signOut}
-        />)
+class FirstLoginPage extends Component {
+  state = {
+    errMsg: null,
+    name: "",
+  }
+
+  changeName = (e) => {
+    this.setState({name: e.target.value});
+  }
+
+  updateProfile = (user) => {
+    user.updateProfile({
+      displayName: this.state.name,
+    }).then(() => {
+      this.props.changePage();
+    }, (error) => {
+      this.setState({errMsg: error})
+    });
+  }
+
+  render() {
+    return(
+      <div className="App">
+      <div className="App-Header">
+      <div className="App-SignIn-Container">
+        <div className="App-Title">
+          ユーザープロフィール
+        </div>
+        <Error error={this.state.errMsg} />
+        <TextBox text={this.state.name} handleChange={this.changeName} placeholder="名前" />
+        <LogInButton text="プロフィール更新" handleClick={(e) => this.updateProfile(this.props.user)} />
+      </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+class VerificationPage extends Component {
+  timer = null;
+
+  constructor(props) {
+    super(props)
+
+    console.log("constructor");
+    console.log(this.props.user);
+    if(this.props.user !== null) {
+      console.log("setInterval");
+      this.timer = setInterval(() => {
+        this.checkVerification(this.props.user)
+      }, 1000)
     }
+  }
 
-    return (
-      <LoginDialog
-        handleLogIn={this.signIn}
-        handleSignUp={this.signUp} />
-    );
+  componentWillUnmount() {
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+    }
+  }
+
+  checkVerification = (user) => {
+    console.log("check");
+    user.reload();
+    if(user.emailVerified) {
+      console.log("changePage");
+      this.props.changePage()
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+      <div className="App-Header">
+      <div className="App-SignIn-Container">
+      <div className="App-Title">
+      ユーザー検証中
+      </div>
+      <div className="App-Description">
+      {this.props.user.email} へメールを送信しました。メールのURLをクリックし本登録を完了してください。登録が完了すると、自動的にページが切り替わります。
+      </div>
+      <div className="App-Dialog-Footer">
+      <LogInButton text="メール再送" handleClick={() => this.props.handleResend(this.props.user)} />
+      </div>
+      </div>
+      </div>
+      </div>
+    )
   }
 }
 
@@ -203,29 +314,29 @@ class TimeCardPage extends Component {
     return (
       <div className="Body">
       <header className="App-header">
-        <div className="App-Main-Container">
-          <StampButton idx={this.state.work_idx}
-           work_states={this.work_states}
-           handleClick={() => this.change_work_state(this.state.work_idx)}
-           user={this.props.user}
-           edit={this.state.edit} />
+      <div className="App-Main-Container">
+      <StampButton idx={this.state.work_idx}
+      work_states={this.work_states}
+      handleClick={() => this.change_work_state(this.state.work_idx)}
+      user={this.props.user}
+      edit={this.state.edit} />
 
-          <WorkStamps edit={this.state.edit}
-           user={this.props.user}
-           works={this.state.works}
-           modified={this.state.modified}
-           handleChange={(e, ki, wi) => {
-              this.newState(ki, wi, moment(e.target.value).valueOf());
-           }}
-           work_states={this.work_states} />
-        </div>
+      <WorkStamps edit={this.state.edit}
+      user={this.props.user}
+      works={this.state.works}
+      modified={this.state.modified}
+      handleChange={(e, ki, wi) => {
+        this.newState(ki, wi, moment(e.target.value).valueOf());
+      }}
+      work_states={this.work_states} />
+      </div>
       </header>
       <Footer
-        onClick={this.props.handleSignOut}
-        onEdit={this.editTime}
-        onDone={this.doneEdit}
-        onCancel={this.cancelEdit}
-        edit={this.state.edit} />
+      onClick={this.props.handleSignOut}
+      onEdit={this.editTime}
+      onDone={this.doneEdit}
+      onCancel={this.cancelEdit}
+      edit={this.state.edit} />
       </div>
     )
   }
@@ -234,8 +345,12 @@ class TimeCardPage extends Component {
 class WorkStamps extends Component {
   render() {
     var {edit, work_states, works, modified, handleChange} = this.props;
-    if(works === null || works.length === 0) {
-      return(<>Loading...</>)
+    if(works === null) {
+      return(<div className="App-ul">Loading...</div>)
+    }
+
+    if(works.length === 0) {
+      return(<div className="App-ul">出退勤情報は<br/>まだありません</div>)
     }
 
     var list = null;
@@ -249,12 +364,12 @@ class WorkStamps extends Component {
       {list.map((elem, i) => {
         return(<div className="App-Couple" key={i}>
           <WorkTime clsName={"App-li-shusha"} idx={0} elem={elem} edit={edit} work_states={work_states}
-            handleChange={(e) => handleChange(e, i, 0)}/>
+          handleChange={(e) => handleChange(e, i, 0)}/>
           <WorkTime clsName={"App-li-taisha"} idx={1} elem={elem} edit={edit} work_states={work_states}
-            handleChange={(e) => handleChange(e, i, 1)}/>
-        </div>)
+          handleChange={(e) => handleChange(e, i, 1)}/>
+          </div>)
       })}
-    </div>)
+      </div>)
   }
 }
 
@@ -263,10 +378,18 @@ class StampButton extends Component {
     timer: null,
   }
 
+  timer = null;
+
   constructor(props) {
     super(props)
 
-    setInterval(this.update, 1000);
+    this.timer = setInterval(this.update, 1000);
+  }
+
+  componentWillUnmount() {
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+    }
   }
 
   update = () => {
@@ -281,12 +404,12 @@ class StampButton extends Component {
       work_idx: this.props.idx,
       date: moment().valueOf(),
     })
-    .then((docRef) => {
-      // console.log("db document writtern with id: ", docRef);
-    })
-    .catch((error) => {
-      console.error("db error adding document: ", error);
-    });
+      .then((docRef) => {
+        // console.log("db document writtern with id: ", docRef);
+      })
+      .catch((error) => {
+        console.error("db error adding document: ", error);
+      });
   }
 
   render() {
@@ -299,9 +422,10 @@ class StampButton extends Component {
     var btnClassName = btn_classes[idx]
     return(
       <button className={btnClassName} onClick={this.stamp}>
-        {work_states[idx]}
-        <div className="App-time">{moment(timer).format("HH:mm:ss")}</div>
-        <div className="App-date">{moment(timer).format("YYYY/MM/DD ddd")}</div>
+      <div className="StampButton-Name">{this.props.user.displayName}</div>
+      {work_states[idx]}
+      <div className="App-time">{moment(timer).format("HH:mm:ss")}</div>
+      <div className="App-date">{moment(timer).format("YYYY/MM/DD ddd")}</div>
       </button>
     )
   }
@@ -312,16 +436,16 @@ const Time = ({elem, edit, handleChange}) => {
   if(edit) {
     return(
       <input className="App-datetime" type="datetime-local"
-       value={moment(elem.date).format("YYYY-MM-DDTHH:mm")}
-       min="2019-01-01T00:00"
-       max="2100-12-31T23:59"
-       onChange={handleChange} />
+      value={moment(elem.date).format("YYYY-MM-DDTHH:mm")}
+      min="2019-01-01T00:00"
+      max="2100-12-31T23:59"
+      onChange={handleChange} />
     )
   } else {
     return(<>
-    <div className="App-time">{moment(elem.date).format("HH:mm")}</div>
-    <div className="App-date">{moment(elem.date).format("YYYY/MM/DD ddd")}</div>
-    </>)
+      <div className="App-time">{moment(elem.date).format("HH:mm")}</div>
+      <div className="App-date">{moment(elem.date).format("YYYY/MM/DD ddd")}</div>
+      </>)
   }
 }
 
@@ -347,10 +471,10 @@ const WorkTime = ({clsName, idx, elem, edit, work_states, handleChange}) => {
 
   return(
     <div className={clsName}>
-      <div className="App-TextLight">{work_states[idx]}</div>
-      <div>
-        <Time elem={info} edit={edit} handleChange={handleChange} />
-      </div>
+    <div className="App-TextLight">{work_states[idx]}</div>
+    <div>
+    <Time elem={info} edit={edit} handleChange={handleChange} />
+    </div>
     </div>
   )
 }
@@ -358,15 +482,14 @@ const WorkTime = ({clsName, idx, elem, edit, work_states, handleChange}) => {
 const LoadingPage = (props) => {
   return (
     <div className="App">
-    <header className="App-header">
-      Loading...
-    </header>
+    <div className="App-Loading">
+    Loading...
+    </div>
     </div>
   )
 }
 
 const LoginDialog = ({
-  handleError,
   handleLogIn,
   handleSignUp
 }) => {
@@ -400,41 +523,88 @@ const LoginDialog = ({
     <div className="App">
     <header className="App-header">
     <div className="App-SignIn-Container">
-      <img className="App-Logo" src={logo_png} alt="logo" />
-      <Error error={errMsg} />
-      <EmailTextBox value={email} handleChange={changeEmailText} />
-      <PassTextBox value={pass} handleChange={changePassText} />
-      <div>
-        <LogInButton text="Log-In" handleClick={logIn} />
-        <SignUpButton text="Sign-Up" handleClick={handleSignUp} />
-      </div>
+    <img className="App-Logo" src={logo_png} alt="logo" />
+    <Error error={errMsg} />
+    <EmailTextBox value={email} handleChange={changeEmailText} />
+    <PassTextBox value={pass} handleChange={changePassText} />
+    <div>
+    <LogInButton text="Log-In" handleClick={logIn} />
+    <SignUpButton text="Sign-Up" handleClick={handleSignUp} />
+    </div>
     </div>
     </header>
     </div>
   )
 }
 
+class SignupPage extends Component {
+  state = {
+    errMsg: null,
+    email: "",
+    pass: "",
+    name: "",
+  }
+
+  changeEmailText = (e) => {
+    this.setState({email: e.target.value});
+  }
+
+  changePassText = (e) => {
+    this.setState({pass: e.target.value});
+  }
+
+  clickRegisterButton = (e) => {
+    var email = this.state.email;
+    var pass = this.state.pass;
+    firebase.auth().createUserWithEmailAndPassword(email, pass).catch((error) => {
+      this.setState({errMsg: error});
+    });
+  }
+
+  render() {
+    return(
+      <div className="App">
+      <div className="App-Header">
+      <div className="App-SignIn-Container">
+      <div>ユーザー登録</div>
+      <Error error={this.state.errMsg} />
+      <EmailTextBox text={this.state.email} handleChange={this.changeEmailText} />
+      <PassTextBox text={this.state.pass} handleChange={this.changePassText} />
+      <LogInButton text="登録" handleClick={this.clickRegisterButton} />
+      </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+const TextBox = ({text, handleChange, placeholder}) => {
+  return(
+    <input className="App-textbox" type="text" value={text} onChange={e => handleChange(e)} placeholder={placeholder}/>
+  )
+}
+
 const EmailTextBox = ({text, handleChange}) => {
   return(
     <input className="App-textbox" type="email" placeholder="E-mail"
-     value={text}
-     onChange={e => handleChange(e)} />
+    value={text}
+    onChange={e => handleChange(e)} />
   )
 }
 
 const PassTextBox = ({text, handleChange}) => {
   return(
     <input className="App-textbox" type="password" placeholder="Password"
-     value={text}
-     onChange={e => handleChange(e)} />
+    value={text}
+    onChange={e => handleChange(e)} />
   )
 }
 
 const LogInButton = ({text, handleClick}) => {
   return(
     <input className="App-signinButton" type="button"
-     value={text}
-     onClick={e => handleClick(e)} />
+    value={text}
+    onClick={e => handleClick(e)} />
   )
 }
 
@@ -453,13 +623,13 @@ const FooterButton = ({text, handleClick}) => {
 const EditButtonGroup = ({handleOnDone, handleOnCancel, handleOnEdit, edit}) => {
   return(<>
     <Visibility condition={true} value={edit}>
-      <FooterButton text="Done" handleClick={handleOnDone} />
-      <FooterButton text="Cancel" handleClick={handleOnCancel} />
+    <FooterButton text="Done" handleClick={handleOnDone} />
+    <FooterButton text="Cancel" handleClick={handleOnCancel} />
     </Visibility>
     <Visibility condition={false} value={edit}>
-      <FooterButton text="Edit" handleClick={handleOnEdit} />
+    <FooterButton text="Edit" handleClick={handleOnEdit} />
     </Visibility>
-  </>)
+    </>)
 }
 
 const Visibility = ({condition, value, children}) => {
@@ -473,16 +643,16 @@ const Visibility = ({condition, value, children}) => {
 const Footer = ({onClick, onEdit, onDone, onCancel, edit}) => {
   return (
     <>
-      <footer className="App-footer" >
-        <FooterButton text="Log-Out" handleClick={onClick} />
-        <EditButtonGroup
-          handleOnDone={onDone}
-          handleOnCancel={onCancel}
-          handleOnEdit={onEdit}
-          edit={edit} />
-      </footer>
-      <div className="App-spacer-for-footer">
-      </div>
+    <footer className="App-footer" >
+    <FooterButton text="Log-Out" handleClick={onClick} />
+    <EditButtonGroup
+    handleOnDone={onDone}
+    handleOnCancel={onCancel}
+    handleOnEdit={onEdit}
+    edit={edit} />
+    </footer>
+    <div className="App-spacer-for-footer">
+    </div>
     </>
   );
 }
@@ -491,7 +661,7 @@ const Error = ({error}) => {
   if(error !== null && error !== undefined) {
     return (
       <div className="App-error">
-        {error.message}
+      {error.message}
       </div>
     )
   } else {
